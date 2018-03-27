0379b19f0b3f3f79882ab9e751b1af6e72ce9d2d
Lorena <lorena.giddens@outlook.com>
2018-03-27 17:03:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // write your code below!
 
 function happyHolidays() {
-  return("Happy holidays!");
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  console.log("Happy holidays, ${name}!");
   return "Happy holidays, ${name}!";
 }
 
