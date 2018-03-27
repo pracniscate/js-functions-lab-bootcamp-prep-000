08f01a14f5a8f9017e9415fa301556ed75811017
Lorena <lorena.giddens@outlook.com>
2018-03-27 17:09:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(you) {
-  console.log("Happy holidays, ${you}!");
-  return "Happy holidays, ${you}!";
+function happyHolidaysTo(name) {
+  console.log("Happy holidays, ${name}");
 }
 
 function happyHolidayTo(holiday, name) {
